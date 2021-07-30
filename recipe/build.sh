829fbad1d439870260ad099a78050c6f20a445fe
Paul Romano <paul.k.romano@gmail.com>
2021-07-30 07:59:59 -0500

    Install cmake config files in lib, not lib64

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,9 +6,10 @@ set -e
 mkdir -p build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DHDF5_ROOT="${PREFIX}" \
-  ..
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DHDF5_ROOT="${PREFIX}" \
+      ..
 make -j "${CPU_COUNT}"
 make install
 cd ..
