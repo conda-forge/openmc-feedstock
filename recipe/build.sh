0562865f40823aa60bf498511d103bec1f1e292a
Jonathan Shimwell <mail@jshimwell.com>
2022-02-21 15:43:33 +0000

    swtiched dagmc on in compile command

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,7 @@ cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_BUILD_TYPE=Release \
       -DHDF5_ROOT="${PREFIX}" \
+      -Ddagmc=ON \
       ..
 make -j "${CPU_COUNT}"
 make install
